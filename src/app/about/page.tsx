0ef7d93d95a77709bbee7a5bc6915a12f2b77c66
yathinm <yathinmrudul@gmail.com>
2025-09-05 23:42:10 -0700

    navbar

---
+++ src/app/about/page.tsx
@@ -0,0 +1,31 @@
+export default function AboutPage() {
+  return (
+    <main
+      style={{
+        width: "100vw",
+        height: "100vh",
+        margin: 0,
+        padding: 0,
+        backgroundColor: "#ffffff",
+        color: "#000",
+        position: "relative",
+      }}
+    >
+      <h1
+        className="title-strong"
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          margin: 0,
+          color: "#3f5a36",
+        }}
+      >
+        About
+      </h1>
+    </main>
+  );
+}
+
+
