e439346c0be6a6027f37d230589b87652abe8ca7
yathinm <yathinmrudul@gmail.com>
2025-09-06 18:22:28 -0700

    vercel web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
             <NavBar />
           </div>
           {children}
+          <Analytics />
         </div>
       </body>
     </html>
