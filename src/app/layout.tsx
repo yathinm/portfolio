b9d5b825cf0d55546fccea250c5f3cf1984f4fed
yathinm <yathinmrudul@gmail.com>
2026-03-03 20:53:58 -0800

    supress hydration warnings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         <div
           style={{
